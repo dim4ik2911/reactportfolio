922485baf09cbfc75e6b0405cc97b6b1764a5239
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-08-25 17:23:02 +0100

    normalize css added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Main = () => (
-  <div className="main-page fade-in">
+  <div className="main-page fade-in-main">
     <h1 className="main-page__title">Dmitrijs Paklons</h1>
     <h3 className="main-page__subtitle">Front-End Developer</h3>
   </div>
