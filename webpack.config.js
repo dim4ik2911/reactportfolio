26773b89c149ff5a392eac7a460ac9af6ee16e78
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-08-27 20:14:35 +0100

    production webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,43 @@
 const path = require("path");
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/,
-      },
-      {
-        loader: "url-loader",
-        test: /\.(png|jpg|gif)$/i,
-      },
-      {
-        loader: "file-loader",
-        test: /\.(pdf|svg)$/,
-      },
-      {
-        test: /\.s?css$/i,
-        //when one loader use loader when more then use "use"
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  devtool: "eval-cheap-module-source-map",
-  devServer: {
-    static: path.join(__dirname, "public"),
-    compress: true,
-    port: 8080,
-    // this attribute means we are going to handle routing through client side
-    historyApiFallback: true,
-  },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
+    },
+    mode: "development",
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          loader: "url-loader",
+          test: /\.(png|jpg|gif)$/i,
+        },
+        {
+          loader: "file-loader",
+          test: /\.(pdf|svg)$/,
+        },
+        {
+          test: /\.s?css$/i,
+          //when one loader use loader when more then use "use"
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
+    },
+    devtool: isProduction ? "source-map" : "eval-cheap-module-source-map",
+    devServer: {
+      static: path.join(__dirname, "public"),
+      compress: true,
+      port: 8080,
+      // this attribute means we are going to handle routing through client side
+      historyApiFallback: true,
+    },
+  };
 };
